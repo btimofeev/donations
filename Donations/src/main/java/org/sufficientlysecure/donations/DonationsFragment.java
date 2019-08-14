4e86d98b9f94598a6846b7c2711b760625a7daa0
penn5 <penn5@users.noreply.github.com>
2019-08-14 13:02:57 +0100

    Update DonationsFragment.java

--- Donations/src/main/java/org/sufficientlysecure/donations/DonationsFragment.java
+++ Donations/src/main/java/org/sufficientlysecure/donations/DonationsFragment.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011-2015 Dominik Schürmann <dominik@dominikschuermann.de>
+ * Copyright (C) 2019 "Hackintosh 5"
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
